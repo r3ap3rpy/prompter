0c8a4289f7418f3a6e5371234afa7f1d70ae2cc2
r3ap3rpy <r3ap3rpy@gmail.com>
2024-12-18 21:35:51 +0000

    Version bumped!

--- prompter/__init__.py
+++ prompter/__init__.py
@@ -1,4 +1,3 @@
-""" Initialize resrouces for import """
-from .prompter import *
+""" Initialize module for import """from .prompter import *
 from .customexceptions import *
-__version__ = '0.0.8'
+__version__ = '0.0.9'
